fd7320027989d8ca1efcd359c9141dd6808abba3
jackwong7777 <jackwong@live.com.my>
2017-04-10 17:38:45 +0800

    Part of the converted C# file errors was fixed

--- BattleshipGame_C#/src/Model/SeaGridAdapter.cs
+++ BattleshipGame_C#/src/Model/SeaGridAdapter.cs
@@ -3,7 +3,6 @@ using Microsoft.VisualBasic;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Data;
 using System.Diagnostics;
 /// <summary>
 /// The SeaGridAdapter allows for the change in a sea grid view. Whenever a ship is
@@ -59,7 +58,7 @@ public class SeaGridAdapter : ISeaGrid
 	/// <summary>
 	/// Indicates that the grid has been changed
 	/// </summary>
-	public event EventHandler ISeaGrid.Changed;
+	public event EventHandler Changed;
 
 	/// <summary>
 	/// Get the width of a tile
--- BattleshipGame_C#/src/UtilityFunctions.cs
+++ BattleshipGame_C#/src/UtilityFunctions.cs
@@ -3,8 +3,8 @@ using Microsoft.VisualBasic;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Data;
 using System.Diagnostics;
+using SwinGameSDK;
 /// <summary>
 /// This includes a number of utility methods for
 /// drawing and interacting with the Mouse.
